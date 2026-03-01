5f4c9faa759c998fc54b6b1f67f68270bc0fd0b5
Pooja <athanepooja313@gmail.com>
2026-03-01 20:01:19 +0530

    the Apple website is done

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { rightImg, watchImg } from '../utils'
+import VideoCarousel from './VideoCarousel'
+
+function Highlights() {
+  useGSAP(() => {
+    gsap.to('#title', {
+      opacity: 1,
+      y: 0,
+    })
+    gsap.to('.link', {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+    })
+
+  }, [])
+
+  return (
+    <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end justify-between md:flex">
+          <h1 id='title' className='section-heading'>Get the highlights.</h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className='link'>
+              Watch the Film
+              <img src={watchImg} alt='watch' />
+            </p>
+            <p className='link'>
+              Watch the event
+              <img src={rightImg} alt='right' />
+            </p>
+            <div className="">
+              <div className="">
+                <VideoCarousel/>
+              </div>
+            </div>
+            
+          </div>
+          
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Highlights
