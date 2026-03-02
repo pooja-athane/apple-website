e9ac411176e4183bccf81fa6d3ac423fad31d8d1
Pooja <athanepooja313@gmail.com>
2026-03-02 11:32:13 +0530

    changed the style of model

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,47 +1,86 @@
-import React from 'react'
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
-import { rightImg, watchImg } from '../utils'
-import VideoCarousel from './VideoCarousel'
+// import React from 'react'
+// import gsap from 'gsap'
+// import { useGSAP } from '@gsap/react'
+// import { rightImg, watchImg } from '../utils'
+// import VideoCarousel from './VideoCarousel'
 
-function Highlights() {
+// function Highlights() {
+//   useGSAP(() => {
+//     gsap.to('#title', {
+//       opacity: 1,
+//       y: 0,
+//     })
+//     gsap.to('.link', {
+//       opacity: 1,
+//       y: 0,
+//       duration: 1,
+//       stagger: 0.25,
+//     })
+
+//   }, [])
+
+//   return (
+//     <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+//       <div className="screen-max-width">
+//         <div className="mb-12 w-full items-end justify-between md:flex">
+//           <h1 id='title' className='section-heading'>Get the highlights.</h1>
+//           <div className="flex flex-wrap items-end gap-5">
+//             <p className='link'>
+//               Watch the Film
+//               <img src={watchImg} alt='watch' />
+//             </p>
+//             <p className='link'>
+//               Watch the event
+//               <img src={rightImg} alt='right' />
+//             </p>
+//             <div className="">
+//               <div className="">
+//                 <VideoCarousel/>
+//               </div>
+//             </div>
+            
+//           </div>
+          
+//         </div>
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default Highlights
+
+
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { rightImg, watchImg } from "../utils"
+
+import VideoCarousel from './VideoCarousel';
+
+const Highlights = () => {
   useGSAP(() => {
-    gsap.to('#title', {
-      opacity: 1,
-      y: 0,
-    })
-    gsap.to('.link', {
-      opacity: 1,
-      y: 0,
-      duration: 1,
-      stagger: 0.25,
-    })
-
+    gsap.to('#title', { opacity: 1, y: 0 })
+    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
   }, [])
 
   return (
-    <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
       <div className="screen-max-width">
-        <div className="mb-12 w-full items-end justify-between md:flex">
-          <h1 id='title' className='section-heading'>Get the highlights.</h1>
+        <div className="mb-12 w-full md:flex items-end justify-between">
+          <h1 id="title" className="section-heading">Get the highlights.</h1>
+
           <div className="flex flex-wrap items-end gap-5">
-            <p className='link'>
-              Watch the Film
-              <img src={watchImg} alt='watch' />
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
             </p>
-            <p className='link'>
+            <p className="link">
               Watch the event
-              <img src={rightImg} alt='right' />
+              <img src={rightImg} alt="right" className="ml-2" />
             </p>
-            <div className="">
-              <div className="">
-                <VideoCarousel/>
-              </div>
-            </div>
-            
           </div>
-          
         </div>
+
+        <VideoCarousel />
       </div>
     </section>
   )
--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -47,24 +47,24 @@ const HowItWorks = () => {
 
         <div className="mt-10 md:mt-20 mb-14">
           <div className="relative h-full flex-center">
-            <div className="overflow-hidden">
+            <div className="overflow-hidden relative">
               <img 
                 src={frameImg}
                 alt="frame"
                 className="bg-transparent relative z-10"
               />
-            </div>
-            <div className="hiw-video">
+              <div className="hiw-video absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[95%] h-[90%] rounded-[56px] overflow-hidden">
                 <video className="pointer-events-none" playsInline preload="none" muted autoPlay ref={videoRef}>
                   <source src={frameVideo} type="video/mp4" />
                 </video>
               </div>
+            </div>
           </div>
           <p className="text-gray font-semibold text-center mt-3">Honkai: Star Rail</p>
           </div>
 
           <div className="hiw-text-container">
-                <div className="flex flex-1 justify-center flex-col">
+                <div className="flex flex-1 justify-center flex-col items-center">
                   <p className="hiw-text g_fadeIn">
                     A17 Pro is an entirely new class of iPhone chip that delivers our {' '}
                     <span className="text-white">
@@ -82,7 +82,7 @@ const HowItWorks = () => {
                 </div>
               
 
-              <div className="flex-1 flex justify-center flex-col g_fadeIn">
+              <div className="flex-1 flex justify-center flex-col g_fadeIn items-center">
                 <p className="hiw-text">New</p>
                 <p className="hiw-bigtext">Pro-class GPU</p>
                 <p className="hiw-text">with 6 cores</p>
